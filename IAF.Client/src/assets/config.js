ab45bd66b3caf82864001e36a2725f439214c8b5
Miguel Glean <migsglen0909@gmail.com>
2026-03-02 11:50:45 +0800

    chnage to 5000

--- IAF.Client/src/assets/config.js
+++ IAF.Client/src/assets/config.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
 
   // Base API URL (change per environment)
-  window.__env.baseUrl = 'https://localhost:5001/api';
+  window.__env.baseUrl = 'https://localhost:5000/api';
 
   // Other environment-specific flags
   window.__env.enableDebug = true;
